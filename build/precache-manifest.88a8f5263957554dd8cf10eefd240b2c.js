90ae96f9af3c6289d50f3d64ab96c606c0b7cd16
appiahboah francisca <68598315+appiahboahfrancisca@users.noreply.github.com>
2020-09-11 00:09:15 +0000

    assignment

---
+++ build/precache-manifest.88a8f5263957554dd8cf10eefd240b2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b52678bf9e058286b0c6909711dd68f",
+    "url": "/Assignment6/index.html"
+  },
+  {
+    "revision": "3cff7a57a16937065365",
+    "url": "/Assignment6/static/css/main.cefb06c1.chunk.css"
+  },
+  {
+    "revision": "c8a91edf015ba30c8ce8",
+    "url": "/Assignment6/static/js/2.b600c56e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Assignment6/static/js/2.b600c56e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cff7a57a16937065365",
+    "url": "/Assignment6/static/js/main.b4de69c8.chunk.js"
+  },
+  {
+    "revision": "a7e276eaf9f0bae3cbf0",
+    "url": "/Assignment6/static/js/runtime-main.e7a4e454.js"
+  }
+]);
---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class HeaderComponent extends Component {
+    render() {
+        return (
+            <div className='header'>
+    <p className='navbar'>GHANA COVID-19 UPDATE INFOMATION</p>
+    </div>
+     );
+    }
+}
+
+export default HeaderComponent;
